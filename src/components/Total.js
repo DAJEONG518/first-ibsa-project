9b78d0db5cbe903775622259ee99702cc8499f3d
조다정 <whekwjd2@gmail.com>
2025-12-17 17:58:23 +0900

    Revert "이미지버그제거완료"
    This reverts commit 25b7a525f0c791e2d6f2c3c6e28315f65a89096c.

---
+++ src/components/Total.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Total.css';
+import Section01 from './Section01'
+import Section02 from './Section02'
+import Section03 from './Section03'
+import Section04 from './Section04'
+import CoverLetter from './CoverLetter'
+
+function Total() {
+  return (
+    <div>
+      <div className='total'>
+        <video 
+          src='./../../public/video/main_1.mp4' muted autoPlay loop 
+          style={{
+            'width':'100%', 
+            'height':'100%', 
+            'objectFit':'cover', 
+            'filter':'brightness(50%)', /* 이미지 어둡게 */
+          }} 
+        />
+        <h1>😃안녕하세요 신입<br />프론트앤드 개발자 입니다</h1>
+      </div>
+        <Section01 />
+        <Section02 />
+        <Section03 />
+        <Section04 />
+        <CoverLetter />
+    </div>
+  )
+} 
+
+export default Total
